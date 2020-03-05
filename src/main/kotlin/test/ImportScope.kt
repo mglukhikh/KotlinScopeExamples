3143bc54193b035281ccd9a469bbeaaf9b6e83a7
Mikhail Glukhikh <glukhikh@mail.ru>
2020-03-05 16:10:34 +0300

    Initial commit

---
+++ src/main/kotlin/test/ImportScope.kt
@@ -0,0 +1,10 @@
+package test
+
+import kotlin.collections.*
+
+fun List<String>.map(f: (String) -> Int): List<Int> = TODO()
+
+fun testImportScope() {
+    listOf("Alpha", "Omega").map { it.toInt() }
+    listOf(1.0, 2.0).map { it.toInt() }
+}
